b659fee9eb6e26c70f060a92ac90eebfaaf7eddd
swetakumari70793-lgtm <swetakumari70793@gmail.com>
2026-01-26 13:32:23 +0530

    new code

--- predict.cpp
+++ predict.cpp
@@ -1,12 +1,13 @@
 #include<iostream>
 using namespace std;
-int main()
+int main ()
 {
     int i;
-    for(i=1;i<=5;i++){
-    if(i==3)
-    break;
-    cout<<"value is : "<<i<<endl;
+    for(i=1;i<=5;i++)
+    {
+        if(i==3)
+        break;
+        cout<<" "<<i;
     }
     return 0;
 }
