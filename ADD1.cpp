a040be404b740ae34b49e1a0be4857bf772beddd
swetakumari70793-lgtm <swetakumari70793@gmail.com>
2026-01-26 14:18:10 +0530

    new add

---
+++ ADD1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a , b, c;
+    cout<<"Enter A value is  : ";
+    cin>>a;
+    cout<<"Enter B value is  : ";
+    cin>>b;
+    c=a+b;
+    cout<<"Add two number value is : "<<c<<endl;
+    return 0;
+}
