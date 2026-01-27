21c70b9a3daa300e86d300f5edede5e3cd358d3a
swetakumari70793-lgtm <swetakumari70793@gmail.com>
2026-01-27 23:48:13 +0530

    new code

---
+++ odd.cpp
@@ -0,0 +1,18 @@
+// peint "odd" if the input value is odd, otherwise print "Even".
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cin>>num;
+    if(num%2!=0)
+    {
+      cout<<"ODD";
+    }
+    else
+    {
+      cout<<"Even";
+    }
+    return 0;
+
+}
