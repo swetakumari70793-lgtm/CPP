c833ee8f4121857b430928cb524b6970cabc6434
swetakumari70793-lgtm <swetakumari70793@gmail.com>
2026-01-28 00:03:05 +0530

    new code

---
+++ Age.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{   int age;
+    cout<<"Age Is :";
+    cin>>age;
+    if(age<12)
+   {
+      cout<<"child Age is :";
+   }
+   else if(age>18)
+   {
+    cout<<"Adult";
+   } 
+   else
+   {
+   cout<<"Tinage";
+   }
+    return 0;
+
+}
---
+++ even_odd.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num ,num1;
+    cin>>num;
+    for(int i=1;i<=10;i++)
+    {
+    if(num%2=0)
+    {
+      cout<<"ODD"<<endl;
+    }
+    else
+    {
+      cout<<"EVEN"<<endl;
+    }
+    }
+    return 0;
+
+}
