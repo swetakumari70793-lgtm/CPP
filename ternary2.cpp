132b68a13ea18eba4652b24ffc90afd2fe46186e
swetakumari70793-lgtm <swetakumari70793@gmail.com>
2026-01-28 20:22:02 +0530

    new code

---
+++ ternary2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int Num;
+    cout<<"Enter number ";
+    cin>>Num;
+    (Num % 2 == 0) ? cout<<" Even Number: "<<Num<<endl : cout<<" Odd Number: "<<Num<<endl ;
+    return 0;
+
+}
