286f24c63bb3f01685d838c91cdb03e1f20de612
swetakumari70793-lgtm <swetakumari70793@gmail.com>
2026-02-01 23:27:54 +0530

    new code

---
+++ Max.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cout<<"A is ";
+    cin>>a;
+    cout<<"B is ";
+    cin>>b;
+    if(a>b)
+    {
+        cout<<"maximum"<<endl;
+    }
+    else
+    {
+        cout<<"minimum"<<endl;
+    }
+    return 0;
+}
