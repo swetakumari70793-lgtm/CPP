c886ed59ece0e4f61f0d2385e4c7411363ad25a9
swetakumari70793-lgtm <swetakumari70793@gmail.com>
2026-02-02 23:30:35 +0530

    new code

---
+++ angle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a , b , c;
+    printf("Enter any number ");
+    scanf("%d",&a);
+    printf("Enter any number ");
+    scanf("%d",&b);
+    c=180-(a+b);
+    printf("Third angle is %d ",c);
+    return 0;
+}
