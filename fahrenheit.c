5fe4a9cfbd5fe0c723d6e82af1a7f3c30bacc107
swetakumari70793-lgtm <swetakumari70793@gmail.com>
2026-02-02 22:38:50 +0530

    new code

---
+++ fahrenheit.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float fahrenheit , celsius ;
+    printf("Enter temperature in fahernheit ");
+    scanf("%f",&celsius);
+    fahrenheit=(fahrenheit-32)*5/9;
+    printf("Fahrenheit = %f",fahrenheit);
+    return 0;
+}
