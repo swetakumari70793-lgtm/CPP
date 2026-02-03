0287883509dabce84eed2382128a21c2d287afe1
swetakumari70793-lgtm <swetakumari70793@gmail.com>
2026-02-03 23:35:00 +0530

    new code

---
+++ add.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a , b , sum;
+    cout<<"Enter Integer value 1st  ";
+    cin>>a;
+    cout<<"Enter Integer value 2nd  ";
+    cin>>b;
+    sum = a + b;
+    cout<<"Sum Integer value is "<<sum<<endl;
+    return 0;
+}
