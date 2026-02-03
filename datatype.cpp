cc78d71b43a2d63d8f1370e7ad2abc9908eb2d3f
swetakumari70793-lgtm <swetakumari70793@gmail.com>
2026-02-03 23:30:07 +0530

    new code

---
+++ datatype.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a = 5500;
+    float b = 77.89;
+    char ch = 'S';
+    double d = 566.8988888888;
+    long int l = 76000000;
+    short int s = 88;
+    cout<<"Integer value is "<<a<<endl;
+    cout<<"Floating value is "<<b<<endl;
+    cout<<"Character value is "<<ch<<endl;
+    cout<<"Double value is "<<d<<endl;
+    cout<<"Long Integer value is "<<l<<endl;
+    cout<<"Short Integer value is "<<s<<endl;
+    return 0;
+}
