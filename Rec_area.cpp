33a5ab8090a2fc29e5461a521e6e95d6f09b397a
swetakumari70793-lgtm <swetakumari70793@gmail.com>
2026-02-04 00:02:44 +0530

    new code

---
+++ Rec_area.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int l , b , area;
+    cout<<"Enter Length  ";
+    cin>>l;
+    cout<<"Enter Breadth ";
+    cin>>b;
+    area = l * b;
+    cout<<"Perimeter of Rectangle is "<<area<<endl;
+    return 0;
+}
