58cfb06a882639be73952e45f955342037a7e4e5
swetakumari70793-lgtm <swetakumari70793@gmail.com>
2026-02-03 23:22:15 +0530

    new code

---
+++ sum5.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a = 20 , b = 15 , add , sub , mul , div ,mod;
+    add = a + b;
+    sub = a - b;
+    mul = a * b;
+    div = a / b;
+    mod = a % b;
+    cout<<"Sum is "<<add<<endl;
+    cout<<"substraction is "<<sub<<endl;
+    cout<<"multipley is "<<mul<<endl;
+    cout<<"divide is "<<div<<endl;
+    cout<<"remainder is "<<mod<<endl;
+    return 0;
+}
