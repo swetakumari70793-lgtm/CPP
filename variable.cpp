1721e3d212c861bf8124568b01cebb9dc40b8f93
swetakumari70793-lgtm <swetakumari70793@gmail.com>
2026-02-04 22:48:55 +0530

    new code

---
+++ variable.cpp
@@ -0,0 +1,18 @@
+//Write a program in C++ to print the sum of two numbers using variables.
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    int b;
+    int sum;
+    cout<<"\n\nPrint the sum of two number using varialbe : \n";
+    cout<<"___________________________________________________\n";
+    
+    a = 20;
+    b = 30;
+    sum = a + b;
+    cout<<"\nThe sum of  "<<a<<" and "<<b<<" is "<<sum<<endl;
+    return 0;
+}
